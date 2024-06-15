ca49b5c7a58adf3ac887cc0ddcd9768def392c40
guille1999utp <90706729+guille1999utp@users.noreply.github.com>
2024-06-15 00:43:09 -0500

    testr 34

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const {format} = require("date-fns");
 
-const handler = async (event) => {
+exports.handler = async (event) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify("la fecha actual es: ",format(new Date(), "yyyy-MM-dd HH:mm:ss")),
   };
   return response;
 };
-
-module.exports =  handler ;
