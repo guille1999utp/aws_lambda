b3364dbee8bfbcd8a87db31e2f8b96e65decb3bd
guille1999utp <90706729+guille1999utp@users.noreply.github.com>
2024-06-15 00:44:55 -0500

    test 45

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {format} = require("date-fns");
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("la fecha actual es: ",format(new Date(), "yyyy-MM-dd HH:mm:ss")),
+    body: JSON.stringify(`la fecha actual es: ${format(new Date(), "yyyy-MM-dd HH:mm:ss")}`),
   };
   return response;
 };
